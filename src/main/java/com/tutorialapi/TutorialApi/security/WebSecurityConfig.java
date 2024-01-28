e665f6975864c58c315713892934c71abb284802
Amospk2 <amospk2@Amospk2.Amospk2.com>
2024-01-28 13:50:28 -0300

    feat: criação de usuários apenas para admin

--- src/main/java/com/tutorialapi/TutorialApi/security/WebSecurityConfig.java
+++ src/main/java/com/tutorialapi/TutorialApi/security/WebSecurityConfig.java
@@ -3,11 +3,13 @@ package com.tutorialapi.TutorialApi.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -57,13 +59,13 @@ public class WebSecurityConfig {
   
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
+    http.csrf(AbstractHttpConfigurer::disable)
         .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> 
-          auth.requestMatchers("/api/auth/**").permitAll()
-              .requestMatchers("/api/test/**").permitAll()
-              .anyRequest().authenticated()
+          auth.requestMatchers(HttpMethod.POST, "/api/auth/signup").permitAll()
+                  .requestMatchers(HttpMethod.POST, "/api/auth/signin").permitAll()
+                  .anyRequest().authenticated()
         );
     
     http.authenticationProvider(authenticationProvider());
